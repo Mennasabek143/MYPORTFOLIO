2c06476c38bf7c249c91f5f3300f820e882f3ff8
Mennasabek143 <mennasabekk@gmail.com>
2024-09-24 21:53:06 +0300

    last update

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ for(let i = 0 ; i<projList.length ; i++ ){
 
 }
 
-//____________dark mode_______________
+//_________________________________dark mode__________________________________________
 
 
 const darkmodebtn = document.getElementById("darkmode");
@@ -30,45 +30,115 @@ const navList =document.getElementsByClassName("a_nav");
 const info =document.getElementById("info");
 
 
-darkmodebtn.addEventListener("click" , function(){
-header.style.background="rgb(29, 29, 126)";
+// darkmodebtn.addEventListener("click" , function(event){
+// event.preventDefault();
+// header.style.background="rgb(29, 29, 126)";
 
-for(let i=0; i<navList.length ; i++) {
-    navList[i].style.background ="black";
-    navList[i].style.color="white"
+// for(let i=0; i<navList.length ; i++) {
+//     navList[i].style.background ="black";
+//     navList[i].style.color="white"
+// }
+
+// main.style.background="black";
+// main.style.color="white";
+
+// darkmodebtn.style.background="black"
+// darkmodebtn.style.color="white"
+
+// info.style.color="rgb(157 181 203)";
+
+// })
+//________________best solution_________________
+function darkmode(){
+    header.style.background="rgb(29, 29, 126)";
+
+    for(let i=0; i<navList.length ; i++) {
+        navList[i].style.background ="black";
+        navList[i].style.color="white"
+    }
+    
+    main.style.background="black";
+    main.style.color="white";
+    
+    darkmodebtn.style.background="black"
+    darkmodebtn.style.color="white"
+    
+    info.style.color="rgb(157 181 203)";
+    
 }
 
-main.style.background="black";
-main.style.color="white";
 
-darkmodebtn.style.background="black"
-darkmodebtn.style.color="white"
-
-info.style.color="rgb(157 181 203)";
-
-})
+//_______________________________more info button_________________________________________
 
 
-//________________________more info button_________________________
-
-
-const moreinfobtn1 = document.getElementById("moreinfo01");
-const moreinfobtn2 = document.getElementById("moreinfo02")
-// const photos =document.getElementsByClassName("card");
-
-
-// moreinfobtn1.addEventListener("click" , function(event){
-//     event.preventDefault();
-// photos[0].style.width="100px";
-// photos[0].style.height="100px";
-
-// })
-
-// moreinfobtn2.addEventListener("click" , function(){
+const moreinfobtn = document.getElementsByClassName("moreinfo")
+const photos =document.getElementsByClassName("cer_photo");
+const photoscont =document.getElementsByClassName("card");
+const moreinfomsg =document.getElementsByClassName("infomsg");
 
 
 
+//____________button1________
+
+        moreinfobtn[0].addEventListener("click" , function(){
+           moreinfobtn[0].style.background="rgb(29, 29, 126)"
+               moreinfobtn[0].style.color="white"
+              if(moreinfobtn[0].innerText==="More information"){
+                moreinfobtn[0].innerText="Less infromation"
+                photos[0].style.display="none"
+                moreinfomsg[0].style.display="inline"
+              }else{
+                moreinfomsg[0].style.display="none"
+                photos[0].style.display="inline"
+                moreinfobtn[0].innerText="More information"
+                moreinfobtn[0].style.background="white"
+                moreinfobtn[0].style.color="rgb(29, 29, 126)"
+              }  
+        })
+        
+//____________button 2______________
+
+moreinfobtn[1].addEventListener("click" , function(){
+    moreinfobtn[1].style.background="rgb(29, 29, 126)"
+        moreinfobtn[1].style.color="white"
+       if(moreinfobtn[1].innerText==="More information"){
+         moreinfobtn[1].innerText="Less infromation"
+          photos[1].style.display="none"
+         moreinfomsg[1].style.display="inline"
+       }else{
+          moreinfomsg[1].style.display="none"
+         photos[1].style.display="inline"
+         moreinfobtn[1].innerText="More information"
+         moreinfobtn[1].style.background="white"
+         moreinfobtn[1].style.color="rgb(29, 29, 126)"
+       }  
+ })
+    
+    
 
 
 
-// })
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
